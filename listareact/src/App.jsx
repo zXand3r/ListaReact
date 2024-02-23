a51bc07d701007ec47220e1e350400ea4e21ea14
xAlessandroo <alessandro.iozzino1@gmail.com>
2024-02-23 19:01:16 +0100

    searchBar and mediaQ

--- listareact/src/App.jsx
+++ listareact/src/App.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { places } from "./places";
 import { Category } from "./Category";
 import "./App.css";
+
 function App() {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const clearSearchText = () => {
+    setSearchText(""); // Imposta il testo di ricerca a una stringa vuota quando si clicca sulla X
+  };
+
   return (
     <div className="mainContainer">
+      <div className="searchBar">
+        <input
+          className="input"
+          type="text"
+          placeholder="Filtra per località..."
+          value={searchText}
+          onChange={handleSearchChange}
+        />
+        {searchText.length > 0 && ( // Mostra la "X" solo quando il testo di ricerca non è vuoto
+          <button className="clearButton" onClick={clearSearchText}>
+            X
+          </button>
+        )}
+      </div>
       <div className="containerLista">
         <h1 className="titoloLista">Lista dei Posti</h1>
         {Object.keys(places).map((category) => (
@@ -12,6 +37,7 @@ function App() {
             key={category}
             category={category}
             cities={places[category]}
+            searchText={searchText}
           />
         ))}
       </div>
--- listareact/src/Category.jsx
+++ listareact/src/Category.jsx
@@ -1,17 +1,27 @@
+import React from "react";
 import { PlaceList } from "./PlaceList";
-import "./Category.css"
+import "./Category.css";
 
-export function Category({ category, cities }) {
-   return (
-       <div className="categoryContainer">
-           {Object.keys(cities).map(city => (
-               <PlaceList
-                   key={`${category}-${city}`}
-                   category={category}
-                   city={city}
-                   places={cities[city]}
-               />
-           ))}
-       </div>
-   );
+export function Category({ category, cities, searchText }) {
+  const filteredCities = Object.keys(cities).filter((city) =>
+    city.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  return (
+    <div className="categoryContainer">
+      {filteredCities.length ? (
+        filteredCities.map((city) => (
+          <PlaceList
+            key={`${category}-${city}`}
+            category={category}
+            city={city}
+            places={cities[city]}
+            searchText={searchText}
+          />
+        ))
+      ) : (
+        <div className="noResults">Error</div>
+      )}
+    </div>
+  );
 }
